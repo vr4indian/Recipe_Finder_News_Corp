fba0c8c06edc6097147b9cd254cec3d9e505a505
vr4indian <vishal.tbl@gmail.com>
2015-04-30 22:57:06 +1000

    Header.php
    Header.php file

---
+++ view/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<link href="css/app.css" rel="stylesheet">
+	<title>Recipe Finder</title>
